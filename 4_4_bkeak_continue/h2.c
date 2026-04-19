3c8d8c3a42d0061675ace8a95650cf3463003348
nvarus <varyukhin.alx@gmail.com>
2026-04-19 20:25:37 +0400

    dfg

--- 4_4_bkeak_continue/h2.c
+++ 4_4_bkeak_continue/h2.c
@@ -1,3 +1,13 @@
-//
-// Created by nvaru on 18.04.2026.
-//
+#include <stdio.h>
+
+int main(void) {
+    char a;
+    char *ptr_a = &a;
+    int *int_ptr_a = ptr_a;
+    *int_ptr_a = 0;
+
+    printf("int_ptr_a: address = %p, value = %d\n", int_ptr_a, *int_ptr_a);
+    printf("ptr_a: address = %p, value = %d\n", ptr_a, *ptr_a);
+
+
+}
